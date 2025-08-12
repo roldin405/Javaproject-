f735af63702ef279ca23934bd13e7cd74a5d96f4
ROLDIN JOSEPH SHAJU <roldinjosephshaju@gmail.com>
2025-08-12 14:42:29 +0530

    package

---
+++ javaproj/main/Main.java
@@ -0,0 +1,13 @@
+
+package main;
+
+import demo.Demo;
+
+public class Main {
+    public static void main(String[] args) {
+        Demo myDemoObject = new Demo();
+
+        System.out.println(myDemoObject.message);
+        myDemoObject.displayMessage();
+    }
+}
