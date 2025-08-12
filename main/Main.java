c0d4a4748aa7a971554520003e9a072e574d48d1
ROLDIN JOSEPH SHAJU <roldinjosephshaju@gmail.com>
2025-08-12 14:49:48 +0530

    package

--- main/Main.java
+++ main/Main.java
@@ -1,14 +1,12 @@
 
 package main;
 
-import demo.Demo; 
+import demo.Demo;
 
 public class Main {
     public static void main(String[] args) {
-        // Create an instance of the Demo class
-        Demo myDemoObject = new Demo();
 
-        // Access the public member variable and method
+        Demo myDemoObject = new Demo();
         System.out.println(myDemoObject.message);
         myDemoObject.displayMessage();
     }
