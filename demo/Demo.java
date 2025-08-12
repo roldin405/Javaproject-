dad37f18e4d1f76c318a4f5e8dc3dffca9c48948
ROLDIN JOSEPH SHAJU <roldinjosephshaju@gmail.com>
2025-08-12 14:46:57 +0530

    package

--- demo/Demo.java
+++ demo/Demo.java
@@ -1,13 +1,11 @@
-// File: MyProject/demo/Demo.java
 
 package demo;
 
 public class Demo {
-    // A public member variable
-    public String message = "Hello from the Demo class in another package!";
 
-    // A public method
+    public String message = "Hello I'm Roldi kunj";
+
     public void displayMessage() {
-        System.out.println("This method is called from outside the 'demo' package.");
+        System.out.println("This method to call 'demo' package.");
     }
 }
